ceef717bd79063939ab46750aa01fbce7e835374
Aish-Borhade <aishwaryaj289@gmail.com>
2024-07-13 18:18:49 +0530

    new file

--- add.java
+++ add.java
@@ -3,8 +3,9 @@ public class Add{
 public int a=10;
 public int b=5;
 
-int sum a+b;
+
 public static void main(String args[])
+int sum a+b;
 { System.out.println (sum);
 }
 
---
+++ subtraction.java
@@ -0,0 +1,9 @@
+public class Substraction
+{
+
+int a=12;
+int b=7;
+int c=a-b;
+
+
+}
